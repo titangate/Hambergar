f382fedb28f0a795de8cd403baaba10aaeccc958
Leon Jiang <titangate1202@gmail.com>
2012-04-13 09:41:56 -0700

    heathaze

--- shader/main.lua
+++ shader/main.lua
@@ -1,8 +1,10 @@
 require 'MiddleClass'
 require 'ShaderEffect'
-require 'shader.shockwave'
-require 'shader.blackhole'
-require 'shader.zoomblur'
+require 'effect.shockwave'
+require 'effect.blackhole'
+require 'effect.zoomblur'
+require 'effect.normal'
+--require 'shader.zoomblur'
 function love.load()
 	img = love.graphics.newImage('demo.jpg')
 	s = ShockwaveEffect()
@@ -13,13 +15,21 @@ function love.load()
 	bh = BlackholeEffect()
 	bh:setParameter{
 		center = {0.5,0.5},
+		radius = 0.3,
 	--	radius = 0.3,
 	}
 	zb = ZoomblurEffect()
 	zb:setParameter{
 		center = {0.5,0.5},
 	}
-	shaders = {s,bh,zb}
+	
+	hazenormal = love.graphics.newImage'effect/heathaze.png'
+	hazenormal:setWrap('repeat','repeat')
+	hz = HeathazeEffect()
+	hz:setParameter{
+		normal = hazenormal,
+	}
+	shaders = {s,bh,zb,hz}
 	id = 1
 end
 

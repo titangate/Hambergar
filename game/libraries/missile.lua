1ac76654554419b0646c66c5ce0d4128b1e8dc0e
Leon Jiang <titangate1202@gmail.com>
2012-04-30 13:41:48 -0700

    porting new missile mechanism

--- game/libraries/missile.lua
+++ game/libraries/missile.lua
@@ -9,34 +9,39 @@ function Missile:initialize(time,mass,vi,x,y,dx,dy,size)
 end
 
 function Missile:createBody(world)
-	self.body = love.physics.newBody(world,self.x,self.y,self.mass,self.mass)
-	self.shape = love.physics.newCircleShape(self.body,0,0,self.size)
+	self.body = love.physics.newBody(world,self.x,self.y,'dynamic')
+	self.shape = love.physics.newCircleShape(self.size)
+	self.fixture = love.physics.newFixture(self.body,self.shape)
+	if self.controller then
+		category,masks = unpack(typeinfo[self.controller])
+		self.fixture:setCategory(category)
+		self.fixture:setMask(unpack(masks))
+	end
 	self.body:setLinearVelocity(self.dx*self.vi,self.dy*self.vi)
 	self.body:setBullet(true)
 	self.body:setAngle(math.atan2(self.dy,self.dx))
-	if self.controller then
-		local category,masks = unpack(typeinfo[self.controller])
-		self.shape:setCategory(category)
-		self.shape:setMask(unpack(masks))
-	end	
-	self.shape:setData(self)
 end
 function Missile:kill()
 	map:removeUnit(self)
 end
+
+
 function Missile:preremove()
-	self.shape:setMask(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+	self.fixture:setMask(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+	
     self.preremoved = true
 end
+
 function Missile:destroy()
     if self.preremoved then
         if self.shape then self.shape:destroy() end
+        if self.fixture then self.fixture:destroy() end
         if self.body then self.body:destroy() end
 		self.shape = nil
+		self.fixture = nil
 		self.body = nil
     end
 end
-
 function Missile:update(dt)
 	self.dt = self.dt + dt
 	self.x,self.y = self.body:getPosition()
